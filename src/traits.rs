32ab0d587ee8ae33e3b6681ce69fc583c536b7f1
fiore <fiore@inventati.org>
2025-02-01 16:50:00 +0100

    mock implementation of the trait

--- src/traits.rs
+++ src/traits.rs
@@ -1,5 +1,11 @@
-use crate::core::NdArray;
+use crate::core::NdArray as Array;
 
 pub trait ArrayLike<T> {
-    fn into_array<const N: usize>(self, shape: &[usize; N]) -> NdArray<T, N>;
+    fn into_array<const N: usize>(self, shape: &[usize; N]) -> Array<T, N>;
+    fn array<const N: usize>(&self, shape: &[usize; N]) -> Array<T, N>;
+}
+
+impl<T, A: AsRef<[T]>> ArrayLike<T> for A {
+    fn into_array<const N: usize>(self, shape: &[usize; N]) -> Array<T, N> {}
+    fn array<const N: usize>(&self, shape: &[usize; N]) -> Array<T, N> {}
 }
