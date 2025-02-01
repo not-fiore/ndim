51a59bb1c692885d18d4a20078cb1dc61535a4de
fiore <fiore@inventati.org>
2025-02-01 17:26:44 +0100

    commented out the generic trait impl

--- src/traits.rs
+++ src/traits.rs
@@ -5,7 +5,8 @@ pub trait ArrayLike<T> {
     fn array<const N: usize>(&self, shape: &[usize; N]) -> Array<T, N>;
 }
 
-impl<T, A: AsRef<[T]>> ArrayLike<T> for A {
-    fn into_array<const N: usize>(self, shape: &[usize; N]) -> Array<T, N> {}
-    fn array<const N: usize>(&self, shape: &[usize; N]) -> Array<T, N> {}
-}
+// TODO: actually write this generic trait impl
+// impl<T, A: AsRef<[T]>> ArrayLike<T> for A {
+//     fn into_array<const N: usize>(self, shape: &[usize; N]) -> Array<T, N> {}
+//     fn array<const N: usize>(&self, shape: &[usize; N]) -> Array<T, N> {}
+// }
